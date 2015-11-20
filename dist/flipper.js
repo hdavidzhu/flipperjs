f14966bedcc74aaca145dcf6d43e4cd76aa3aa16
David Zhu <hdavidzhu@gmail.com>
2015-11-20 17:44:37 -0500

    UPDATE: Gulpfile.

---
+++ dist/flipper.js
@@ -0,0 +1,20 @@
+function inverseObj(originalObj) {
+    var inversedObj = {};
+    var resultingValue;
+    // First, we want to loop through all the keys of our input object.
+    Object.keys(originalObj).map(function (originalObjKey, index) {
+        resultingValue = originalObj[originalObjKey];
+        // For the values of each key, check if it is an array.
+        if (Object.prototype.toString.call(resultingValue) === '[object Array]') {
+            // If it is an array, let's loop through the (string) values and set them
+            // as keys for the inversed Object.
+            resultingValue.map(function (result) {
+                inversedObj[result] = originalObjKey;
+            });
+        }
+        else {
+            inversedObj[resultingValue] = originalObjKey;
+        }
+    });
+    return inversedObj;
+}
---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// IMPORTS =====================================================================
+
+var gulp        = require('gulp');
+var ts          = require('gulp-typescript');
+var mocha       = require('gulp-mocha');
+var tsConfig    = require('./tsconfig.json');
+var srcGlob     = 'src/**/*.ts';
+
+
+
+// TASKS =======================================================================
+
+gulp.task('typescript', function() {
+  return gulp.src(srcGlob)
+    .pipe(ts(tsConfig))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('typescript:tdd', function() {
+  gulp.watch(srcGlob, ['typescript']);
+});
+
+gulp.task('test', function() {
+  return gulp
+    .src('spec/**/*.js', {read: false})
+    .pipe(mocha({
+      reporter: 'spec'
+    }));
+});
+
+gulp.task('test:tdd', function() {
+  gulp.watch(srcGlob, ['test']);
+});
+
+
+
+// GROUPS ======================================================================
+
+gulp.task('build'  , ['typescript', 'test']);
+gulp.task('default', ['build', 'typescript:tdd', 'test:tdd']);
