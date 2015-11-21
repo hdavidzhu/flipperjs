fdb54d69b24a0a69b9324d7feffeca094ada21a1
JOHN QUINN <JohnQuinn93@gmail.com>
2015-11-20 23:39:16 -0500

    ADD: Spec setup and first spec for Flipper.

--- spec/flipper_spec.js
+++ spec/flipper_spec.js
@@ -1,1 +1,39 @@
 // var flippers = require()
+
+sinon = require("sinon");
+
+var chai = require('chai');
+var sinonChai = require("sinon-chai");
+var Flipper = require('../dist/flipper').Flipper;
+
+expect = chai.expect;
+chai.use(sinonChai);
+
+describe("Flipper", function() {
+  describe("flip", function() {
+
+    var jqObject;
+    var jqFlip;
+
+    beforeEach(function() {
+      jqObject = {
+        cheese: 'brie',
+        bread: 'rye',
+        jelly: 'lingenberry',
+        board: 'mahogony'
+      }
+
+      jqFlip = {
+        brie: 'cheese',
+        rye: 'bread',
+        lingenberry: 'jelly',
+        mahogony: 'board'
+      }
+    });
+
+    it("can flip a basic object with a one-to-one link", function() {
+      var result = Flipper.flip(jqObject);
+      expect(result).to.deep.equal(jqFlip);
+    });
+  });
+});
