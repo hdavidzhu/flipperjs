4b207549e494e502568d67457b7cd51f8d99f232
JOHN QUINN <JohnQuinn93@gmail.com>
2015-11-21 13:02:34 -0500

    ADD: helper

--- dist/flipper.js
+++ dist/flipper.js
@@ -2,20 +2,25 @@ var _Flipper = (function () {
     function _Flipper() {
     }
     _Flipper.prototype.flip = function (originalObj) {
+        var _this = this;
         var inversedObj = {};
         var resultingValue;
         Object.keys(originalObj).map(function (originalObjKey, index) {
             resultingValue = originalObj[originalObjKey];
             if (Object.prototype.toString.call(resultingValue) === '[object Array]') {
                 resultingValue.map(function (result) {
-                    inversedObj[result] = originalObjKey;
+                    _this._setValueToKey(inversedObj, result, originalObjKey);
                 });
             }
             else {
                 inversedObj[resultingValue] = originalObjKey;
+                _this._setValueToKey;
             }
         });
         return inversedObj;
+    };
+    _Flipper.prototype._setValuetoKey = function (inputObj, key, value) {
+        inputObj[key] = value;
     };
     return _Flipper;
 })();
--- src/flipper.ts
+++ src/flipper.ts
@@ -7,7 +7,7 @@ class _Flipper {
     var resultingValue : any;
 
     // First, we want to loop through all the keys of our input object.
-    Object.keys(originalObj).map(function(originalObjKey, index) {
+    Object.keys(originalObj).map((originalObjKey, index) => {
 
       resultingValue = originalObj[originalObjKey];
 
@@ -16,17 +16,23 @@ class _Flipper {
 
         // If it is an array, let's loop through the (string) values and set them
         // as keys for the inversed Object.
-        resultingValue.map(function(result : string) {
-          inversedObj[result] = originalObjKey;
+        resultingValue.map((result : string) => {
+          this._setValueToKey(inversedObj, result, originalObjKey);
         });
 
       // Otherwise, set it directly.
       } else {
         inversedObj[resultingValue] = originalObjKey;
+        this._setValueToKey
       }
     });
 
     return inversedObj;
+
+    }
+
+  private _setValuetoKey(inputObj : any, key : string, value : string) {
+    inputObj[key] = value;
   }
 }
 
